3f7c4c5eb550eab32b8ca0cab1cd6df580d360c7
ArjunDhruva <123874852+ArjunDhruva@users.noreply.github.com>
2026-04-17 02:56:51 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,11 +3,11 @@ import axios from "axios";
 import { API_BASE_URL } from "../config";
 
 const authApi = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 export const registerUser = (payload) =>
-  authApi.post("/api/auth/signup", payload);
+  authApi.post("/auth/signup", payload);
 
 export const loginUser = (payload) =>
-  authApi.post("/api/auth/login", payload);
+  authApi.post("/auth/login", payload);

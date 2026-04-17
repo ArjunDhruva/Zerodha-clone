63ff41dae9650307cb316af21d74e3976fe5256c
ArjunDhruva <123874852+ArjunDhruva@users.noreply.github.com>
2026-04-17 12:48:53 +0530

    Comment out Dashboard link in Navbar
    Comment out the Dashboard link in the Navbar.

--- frontend/src/LandingPage/Navbar.js
+++ frontend/src/LandingPage/Navbar.js
@@ -50,11 +50,11 @@ function Navbar() {
                 Support
               </Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link active" href={getDashboardUrl("/login")}>
-                Dashboard
-              </a>
-            </li>
+            // <li className="nav-item">
+            //   <a className="nav-link active" href={getDashboardUrl("/login")}>
+            //     Dashboard
+            //   </a>
+            // </li>
           </ul>
         </div>
       </div>
